fec38069e0c2b5227700b3741bbc7f3a95ce8472
swbradshaw <scott@chillaxing.com>
2015-11-17 09:26:18 -0500

    Enhancements
    - Pull version from iOS package
    - Show app type (iOS, Android, etc) on the Jenkins config screen

--- src/main/java/com/apperian/eas/metadata/IOSMetadataExtractor.java
+++ src/main/java/com/apperian/eas/metadata/IOSMetadataExtractor.java
@@ -3,6 +3,7 @@ package com.apperian.eas.metadata;
 import java.io.File;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -19,32 +20,80 @@ public class IOSMetadataExtractor extends MetadataExtractor {
     @Override
     public boolean tryExtractTo(Metadata metadata, File file, PrintStream logger) {
         this.jenkinsLogger = logger;
+        boolean extracted = false;
         try (ZipFile zip = new ZipFile(file)) {
-            ZipEntry entry = zip.getEntry("iTunesMetadata.plist");
-            if (entry == null) {
-                return false;
-            }
-            try (InputStream in = zip.getInputStream(entry)) {
-                NSDictionary dict = (NSDictionary) PropertyListParser.parse(in);
-
-                extractProperties(metadata, dict);
-            }
+        	// try iTunesMetadata.plist first        	
+        	extracted = extractiTunesMetadata(zip, metadata);
+        	if (!extracted) {
+        		// try Info.plist        		
+        		extracted = extractInfoMetadata(zip, metadata);
+        	}
+        	if (!extracted) {
+        		this.jenkinsLogger.println("Unable to find metadata inside the app");
+        	}
         } catch (Exception ex) {
             ex.printStackTrace();
             return false;
         }
+        return extracted;
+    }
+    
+    private ZipEntry findFileinZip(String name, ZipFile file)
+    {
+    	for (Enumeration<? extends ZipEntry> e = file.entries();  e.hasMoreElements();) {
+    		ZipEntry zipEntry = e.nextElement();
+	        if (zipEntry.getName().endsWith(name))
+	          return zipEntry;
+    	}
+      return null;
+    }
+
+    private boolean extractInfoMetadata(ZipFile zip, Metadata metadata) {
+    	ZipEntry entry = findFileinZip("Info.plist", zip);
+        if (entry == null) {
+            return false;
+        }
+        this.jenkinsLogger.println("Found Info.plist in iOS app");        
+        try (InputStream in = zip.getInputStream(entry)) {
+            NSDictionary dict = (NSDictionary) PropertyListParser.parse(in);
+
+            String bundleDisplayName = getCfg(dict, "CFBundleDisplayName","CFBundleName");            
+            String bundleVersion = getCfg(dict, "CFBundleVersion");
+
+            setAndLog(metadata, KnownFields.NAME, bundleDisplayName);            
+            setAndLog(metadata, KnownFields.VERSION, bundleVersion);
+        } catch (Exception ex) {
+        	this.jenkinsLogger.println(ex.toString());
+            ex.printStackTrace();
+            return false;
+        }
         return true;
-    }
+   }
 
-    private void extractProperties(Metadata metadata, NSDictionary cnf) {
-        String bundleDisplayName = getCfg(cnf, "itemName", "bundleDisplayName", "playlistName");
-        String artistName = getCfg(cnf, "artistName");
-        String bundleVersion = getCfg(cnf, "bundleVersion");
+    
+    private boolean extractiTunesMetadata(ZipFile zip, Metadata metadata) {
+    	 ZipEntry entry = findFileinZip("iTunesMetadata.plist", zip);    	 
+         if (entry == null) {
+             return false;
+         }         
+         this.jenkinsLogger.println("Found iTunesMetadata.plist in iOS app");
+         try (InputStream in = zip.getInputStream(entry)) {
+             NSDictionary dict = (NSDictionary) PropertyListParser.parse(in);
 
-        setAndLog(metadata, KnownFields.NAME, bundleDisplayName);
-        setAndLog(metadata, KnownFields.AUTHOR, artistName);
-        setAndLog(metadata, KnownFields.VERSION, bundleVersion);
+             String bundleDisplayName = getCfg(dict, "itemName", "bundleDisplayName", "playlistName");
+             String artistName = getCfg(dict, "artistName");
+             String bundleVersion = getCfg(dict, "bundleVersion");
+
+             setAndLog(metadata, KnownFields.NAME, bundleDisplayName);
+             setAndLog(metadata, KnownFields.AUTHOR, artistName);
+             setAndLog(metadata, KnownFields.VERSION, bundleVersion);
+         } catch (Exception ex) {
+             ex.printStackTrace();
+             return false;
+         }
+    	return true;
     }
+    
 
     private String getCfg(NSDictionary cnf, String ...props) {
         for (String prop : props) {
