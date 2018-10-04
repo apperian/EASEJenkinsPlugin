b95582a68758c8f6cd962da42a4ee5674b8b0ada
Andrew Finley <afinley@apperian.com>
2018-10-04 15:24:40 -0400

    APP-19305:  Fix the GET Policies response objects

--- src/main/java/com/apperian/api/applications/GetPoliciesResponse.java
+++ src/main/java/com/apperian/api/applications/GetPoliciesResponse.java
@@ -1,21 +1,46 @@
 package com.apperian.api.applications;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class GetPoliciesResponse {
-    private int statusCode;
-    private String description;
+    @JsonProperty("status")
+    private PolicyStatus policyStatus;
+
+    @JsonProperty("configurations")
     private List<PolicyConfiguration> policyConfigurations;
 
-    public int getStatusCode() {
-        return statusCode;
-    }
+    @JsonProperty("previous_version_configurations")
+    private List<PolicyConfiguration> previousVersionConfigurations;
 
-    public String getDescription() {
-        return description;
+    public PolicyStatus getPolicyStatus() {
+        return policyStatus;
     }
 
     public List<PolicyConfiguration> getPolicyConfigurations() {
         return policyConfigurations;
+    }
+
+    public List<PolicyConfiguration> getPreviousVersionConfigurations() {
+        return previousVersionConfigurations;
+    }
+
+    // NOTE:  Even though the 'PolicyStatus' class is only used internally here, we need it to be public so that the
+    //        mapper can map the JSON to it.
+    public class PolicyStatus {
+        @JsonProperty("code")
+        int code;
+
+        @JsonProperty("description")
+        String description;
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getDescription() {
+            return description;
+        }
     }
 }
--- src/main/java/com/apperian/api/applications/PolicyConfiguration.java
+++ src/main/java/com/apperian/api/applications/PolicyConfiguration.java
@@ -1,11 +1,19 @@
 package com.apperian.api.applications;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import net.sf.json.JSONObject;
 
 public class PolicyConfiguration {
+    @JsonProperty("policy_id")
     String policyId;
+
+    @JsonProperty("configuration")
     JSONObject configuration;
+
+    @JsonProperty("id")
     String id;
+
+    @JsonProperty("auth_required")
     String authRequired;
 
     public String getPolicyId() {
