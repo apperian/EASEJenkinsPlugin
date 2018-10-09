ea5376cf048249396a66c0083dcfde196c10a333
purigarcia <pgarcia@apperian.com>
2018-10-09 14:04:11 +0200

    Remove support to windows phone app and added for appx

--- src/main/java/com/apperian/api/applications/AppType.java
+++ src/main/java/com/apperian/api/applications/AppType.java
@@ -3,10 +3,10 @@ package com.apperian.api.applications;
 public enum AppType {
     IOS(0, "iOS"),
     ANDROID(3, "Android"),
-    WIN_PHONE(10, "Windows Phone"),
     WIN_EXE(12, "Windows EXE"),
     WIN_MSI(13, "Windows MSI"),
-    WIN_ZIP(14, "Windows ZIP");
+    WIN_ZIP(14, "Windows ZIP"),
+    WIN_APPX(15, "Windows APPX");
 
     final int ordValue;
     final String displayName;
